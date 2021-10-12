494653b16b74725c28bcaf6d83aa29569900f968
126saransh <61222856+126saransh@users.noreply.github.com>
2021-10-12 12:09:44 +0530

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,11 @@
+# Q. Write a program to greet all the person names stored in a list L1 and which starts with S. (hint: use startswith("S") method)   L1 = [“Simarjeet”, “Sohan”, “Sachin”, “Rahul”]
+
+L1 = ["Simar", "Mayank", "Sahil", "Rahul"]
+
+for i in L1:
+    if i.startswith('S'):
+        print("Hello ",i)
+
+
+# Output : Hello  Simar
+#          Hello  Sahil
